3cb5f42f83922b8b5a5f355c265fcdb6264a3b33
Zeryntth <dev@zerynth.com>
2020-04-24 13:10:53 +0000

    r2.5.1-base

--- can.py
+++ can.py
@@ -7,7 +7,11 @@ CAN
 
 This module loads the Controller Area Network (CAN) driver.
 
-(TODO: brief intro about CAN and this driver functions)
+A Controller Area Network (CAN bus) is a robust vehicle bus standard designed to allow microcontrollers and devices to communicate with each other's applications without a host computer.
+It is a message-based protocol, designed originally for multiplex electrical wiring within automobiles to save on copper, but can also be used in many other contexts. 
+For each device the data in a frame is transmitted sequentially but in such a way that if more than one device transmits at the same time the highest priority device is able to continue while the others back off.
+Frames are received by all devices, including by the transmitting device.
+
     """
 
 
