9e3dd9481c8b025c12efe27488656f23afdcdd9f
Lorenzo Cappelletti <lorenzo.cappelletti@gmail.com>
2021-02-21 22:28:22 +0100

    Add built-in function id()

--- csrc/mcu/mcu.c
+++ csrc/mcu/mcu.c
@@ -1,5 +1,10 @@
 #include "zerynth.h"
 
+C_NATIVE(__builtin_id){
+  NATIVE_UNWARN();
+  *res = PSMALLINT_NEW((IS_TAGGED(args[0]) ? &args[0] : args[0]));
+  return ERR_OK;
+}
 
 C_NATIVE(_mcu_reset){
   NATIVE_UNWARN();
